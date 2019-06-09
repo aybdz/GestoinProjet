0dce591b5cf6c8514e4554ca2e4de057abfa618a
Aybdz <a.89@hotmail.fr>
2019-06-09 15:11:29 +0100

    initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+use Auth;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+    	if (Auth::check()) {
+    		return view('welcome');
+    	}else{
+    		return view('login');
+    	}
+    }
+    public function dashboard()
+    {
+    	return view('welcome');
+    }
+}
